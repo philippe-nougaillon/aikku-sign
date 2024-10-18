cbfdc4e00e3d151b2cb53e463830d2cbfaad9c6c
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2024-10-18 15:26:01 +0200

    assets path & some more fix

--- app/pdfs/assemblee_pdf.rb
+++ app/pdfs/assemblee_pdf.rb
@@ -35,7 +35,7 @@ class AssembleePdf
         move_down @margin_down * 25
         image "#{@image_path}/Aikku_logo", :width => 25, position: :center
         move_down @margin_down
-        text "<u><a href='https://emargements.philnoug.com/'>Convocation réalisée gratuitement</a></u>", align: :center, inline_format: true
+        text "<u><a href='https://sign.aikku.eu/'>Convocation réalisée gratuitement</a></u>", align: :center, inline_format: true
       end
     end
   end
